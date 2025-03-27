fe25461569d26c2f8015209ee0aa8dd81aed8f76
Edwin <112639511+Edwinmg27@users.noreply.github.com>
2025-03-27 18:30:10 -0400

    Add Variables privates and wildcards
    agregando descripción de variables privadas y variables wildcards

--- 01_Variables_Datatypes.dart
+++ 01_Variables_Datatypes.dart
@@ -5,10 +5,14 @@ final double pi = 3.1416; //* final: asignación única en tiempo de ejecución
 const bool isActive = true; //* const: valor constante en tiempo de compilación
 dynamic variableDinamica =
     "Texto"; //* dynamic: Tipo cambiante en tiempo de ejecución (evitar si no es necesario).
-late //* Tiene dos casos de uso: quieres declarar una variable no nullable, pero no puedes inicializarla de inmediato.
+
+late String description;
+//* Tiene dos casos de uso: 1) quieres declarar una variable no nullable, pero no puedes inicializarla de inmediato.
+//* 2) Inicialización Perezosa (Lazy Initialization) y Inicialización Retardada (Delayed Initialization).
+//* No puedes usar late con variables nullable:
+/******************************************************************************************************************************/
 //* TIPOS PRIMITIVOS
-int
-edad = 30; //* int se usa para declarar variables de tipo enteros de 64bits
+int edad = 30; //* int se usa para declarar variables de tipo enteros de 64bits
 
 double precio =
     4.99; //* double se usa para declarar e inicializar variables de Números de puntos flotantes
@@ -23,3 +27,13 @@ bool isActivo =
 String? direccion =
     null; //* Null Safety: permite añadir datos nulables para prevenir errores comunes causados por variables con valores NULL.
 //*mejorando la seguridad  robustez del código.
+
+//* WILDCARDS VARIABLES
+//* suele asociarse al uso del guion bajo (_) como convención para ignorar variables o valores no utilizados en ciertos contextos.
+void mostrarMensaje(String mensaje, int _, bool __) {
+  print(mensaje); // Los otros parámetros se marcan con _ y __ para ignorarlos.
+}
+
+//*Variables privadas
+//* Se declara anteponiendo un guion bajo (_) al nombre de la variable. La privacidad en Dart es a nivel de biblioteca (archivo)
+String _variablePrivada = "Secreto";
